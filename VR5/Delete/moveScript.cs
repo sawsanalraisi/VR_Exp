291cc3ac01ac14757bc7cf59dd986807024378ab
sawsanalraisi <44733946+sawsanalraisi@users.noreply.github.com>
2019-11-04 17:04:41 +0400

    vr5

---
+++ VR5/Delete/moveScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveScript : MonoBehaviour
+{
+
+
+     void Update()
+    {
+        //transform.position += transform.forward * Time.deltaTime * 90.0f;
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+    }
+
+
+
+}
